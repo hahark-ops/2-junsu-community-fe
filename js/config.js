e78ff43771cf4e32f04ecec75c7e207a919987ed
황준수 <hahark@naver.com>
2026-02-12 14:05:59 +0900

    chore: Adjust application configuration settings.

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // 배포 환경별 API 서버 주소를 이 파일에서 관리합니다.
 // S3 정적 배포 시 이 값을 BE 주소로 설정하세요. 예) http://43.200.x.x:8000
 window.RUNTIME_CONFIG = window.RUNTIME_CONFIG || {};
-window.RUNTIME_CONFIG.API_BASE_URL = window.RUNTIME_CONFIG.API_BASE_URL || '';
+window.RUNTIME_CONFIG.API_BASE_URL = "http://15.164.166.84:8000";
